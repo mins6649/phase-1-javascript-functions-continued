705b3eb72888f1e600fd92a095b15b59b9cd6b6f
minyoungshin <mins6649@gmail.com>
2023-01-31 17:08:19 -0500

    outer-inner function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}  
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(specialKey = '*'){
+    return function(characteristic = "special"){
+        return `You are ${specialKey}${characteristic}${specialKey}!`
+
+    }
+}
